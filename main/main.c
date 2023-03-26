fc7c0c9bc442f0a38829d6c8bda77628378651be
EspenTeigen <espen.teigen@hotmail.com>
2023-03-26 22:14:22 +0200

    added the projectile test

--- main/main.c
+++ main/main.c
@@ -1,8 +1,49 @@
 #include <stdio.h>
 #include "tuples.c"
 
-int main(){
-printf("test ikke faen\n");
+void projectile_path(void);
 
+int main(){
+    projectile_path();
 return 0;
+}
+
+
+void projectile_path(void){
+
+    struct _projectile{
+        point3 position;
+        vect3 velocity;
+    }projectile;
+
+    struct _environment{
+        vect3 gravity;
+        vect3 wind;
+    }environment;
+
+    // Set up initial conditions
+    projectile.position = point(0.0f, 1.0f, 0.0f);
+    projectile.velocity = vector(10.0f, 10.0f, 0.0f);
+
+    // Set up environmental factors
+    environment.gravity = vector(0.0f, -0.1f, 0.0f);
+    environment.wind = vector(-0.1f, 0.0f, 0.0f);
+
+    int tick = 0;
+
+    while(projectile.position.y >= 0){
+        tick += 1;
+        projectile.position = add_tuple(&projectile.position, &projectile.velocity);
+        projectile.velocity = add_tuple(&projectile.velocity, &environment.gravity);
+        projectile.velocity = add_tuple(&projectile.velocity, &environment.wind);
+
+        printf("x = %f, y = %f, z=%f, ticks = %i\n", 
+            projectile.position.x,  
+            projectile.position.y, 
+            projectile.position.z, 
+            tick);
+    }
+
+
+
 }
