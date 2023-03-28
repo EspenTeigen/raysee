ac111eab38fe0ea7b780505aea0114b2bb636925
EspenTeigen <espen.teigen@hotmail.com>
2023-03-28 22:34:16 +0200

    changed file hierarky

--- clean.sh
+++ clean.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
 
-cd build
-rm -r CMakeFiles
-rm -r external
-rm -r main
-rm -r test
-rm -r lib
-rm -r Testing
-rm  .ninja_deps
-rm  .ninja_log
-rm  build.ninja
-rm  cmake_install.cmake
-rm  CMakeCache.txt
-rm  CTestTestfile.cmake
-rm  DartConfiguration.tcl
+cd build/*
+
