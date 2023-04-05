ee805a41a9202b4cd79d268c0c54433d63c1b8ef
EspenTeigen <espen.teigen@hotmail.com>
2023-04-05 23:46:15 +0200

    started with the transofmrations library

---
+++ lib/transformations.h
@@ -0,0 +1,6 @@
+#ifndef TRANSFORMATIONS_H_
+#define TRANSFORMATIONS_H_
+#include "tuples.h"
+#include "matrices.h"
+
+#endif
---
+++ test/transformations_test.c
@@ -0,0 +1,19 @@
+#include <unity.h>
+#include <stdlib.h>
+#include <matrices.h>
+#include <transformations.h>
+
+void setUp(void) {
+    
+}
+
+
+void tearDown(void) {
+    // clean stuff up here
+}
+
+int main(void) {
+    UNITY_BEGIN();
+
+    return UNITY_END();
+}
