f625415a0c67c15df35f5078f6ff3509f2af7200
EspenTeigen <espen.teigen@hotmail.com>
2023-04-06 23:17:22 +0200

    added some comments

--- lib/canvas.h
+++ lib/canvas.h
@@ -4,15 +4,35 @@
 #include <stddef.h>
 #include <tuples.h>
 
-
+/// @brief used to store the canvas
 typedef struct _canvas_t{
+    /// @brief pixel width
     long width;
+    /// @brief pixel height
     long height;
+    /// @brief the pixels
     color_t pixels[];
 }canvas_t;
 
+/// @brief Create a canvas
+/// @param width pixel width
+/// @param height pixel height
+/// @return a pointer to the canvas
 canvas_t* canvas_create(const long width, const long height);
+
+/// @brief free the malloced canvas
+/// @param canvas the canvas to free
 void canvas_delete(canvas_t **canvas);
+
+/// @brief Write a pixel to the canvas
+/// @param canvas pointer to the canvas created by canvas_create
+/// @param x x-pixel to set
+/// @param y y pixel to set
+/// @param color the color to set the pixel to
 void canvas_write_pixel(canvas_t *canvas, long x, long y, color_t *color);
+
+/// @brief Create image in ppm format
+/// @param canvas pointer the canvas created by canvas_create
+/// @param canvas_name name of canvas
 void canvas_to_ppm(canvas_t *canvas, const char* canvas_name);
 #endif // _CANVAS_H_
--- lib/matrices.h
+++ lib/matrices.h
@@ -2,23 +2,80 @@
 #define MATRICES_H_
 #include "tuples.h"
 
+/// @brief 2x2 matrix
 typedef double matrix2[2][2];
+
+/// @brief 3x3 matrix
 typedef double matrix3[3][3];
+
+/// @brief 4x4 matrix
 typedef double matrix4[4][4];
 
+/// @brief Check if two 2x2 matrices are equal
+/// @param mat1 first matrix
+/// @param mat2 second matrix
+/// @return true if equal
 bool matrix2_equal(matrix2 mat1, matrix2 mat2);
+
+/// @brief finds determinant 
+/// @param a the 2x2 matrix
+/// @return the determinant
 double matrix2_det(matrix2 a);
 
+/// @brief check if two 3x3 matrices are equal
+/// @param mat1 first matrix
+/// @param mat2 second matrix
+/// @return true if equal
 bool matrix3_equal(matrix3 mat1, matrix3 mat2);
+
+/// @brief get the submatrix of a 3x3 matrix
+/// @param a matrix3: to get submatrix from
+/// @param row row to remove
+/// @param col column to remove
+/// @param res a 2x2 submatrix
 void matrix3_submatrix(matrix3 a, const int row, const int col, matrix2 res);
+
+/// @brief finds the minor of a 3x3 matrix
+/// @param a matrix3: the 3x3 matrix
+/// @param row row to exclude when finding the minor
+/// @param col column to exclude when finding the minor
+/// @return the minor of a 3x3 matrix
 double matrix3_minor(matrix3 a, const int row, const int col);
+
+/// @brief 
+/// @param a 
+/// @param row 
+/// @param col 
+/// @return 
 double matrix3_cofactor(matrix3 a, const int row, const int col);
+
+/// @brief get determinant
+/// @param a matrix3: matrix to get determinant from
+/// @return the determinant
 double matrix3_det(matrix3 a);
 
+/// @brief Check if two 4x4 matrices are equal
+/// @param mat1 matrix4
+/// @param mat2 matrix4
+/// @return true if equal
 bool matrix4_equal(matrix4 mat1, matrix4 mat2);
+
+/// @brief Multiply two 4x4 matrices
+/// @param mat1 matrix4
+/// @param mat2 matrix4
+/// @param res result of operation
 void matrix4_mult(matrix4 mat1, matrix4 mat2, matrix4 res);
+
+/// @brief Multiply a 4x4 matrix with a vector
+/// @param mat matrix4 matrix to multiply with
+/// @param vect vect3 vector to multiply with
+/// @param res vect3 containing result
 void matrix4_vect3_mult(matrix4 mat, vect3 vect, vect3 res);
+
+/// @brief get a 4x4 identity matrix
+/// @param res matrix4: the identity matrix returned
 void matrix4_get_identity(matrix4 res);
+
 void matrix4_submatrix(matrix4 a, const int row, const int col, matrix3 res);
 double matrix4_minor(matrix4 a, const int row, const int col);
 double matrix4_cofactor(matrix4 a, const int row, const int col);
